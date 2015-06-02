3783ed839c3483e2a3c31f12a035f4b07b6b75b1
unknown <pat@fik01pcr3440.finn-power.com>
2015-06-02 15:13:23 +0300

    BatchChecker

---
+++ BatchChecker/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Messaging;
+using BatchInspector.ViewModel;
+
+namespace BatchInspector
+{
+    /// <summary>
+    /// This application's main window.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+
+            this.Left = Properties.Settings.Default.WindowLeft;
+            this.Top = Properties.Settings.Default.WindowTop;
+        }
+
+        public void OnDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.All;
+            e.Handled = true;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                Messenger.Default.Send<string[], MainViewModel>(files);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_LayoutUpdated(object sender, System.EventArgs e)
+        {
+            Properties.Settings.Default.WindowHeight = this.Height;
+            Properties.Settings.Default.WindowWidth = this.Width;
+        }
+
+        private void Window_LocationChanged(object sender, System.EventArgs e)
+        {
+            Properties.Settings.Default.WindowLeft = this.Left;
+            Properties.Settings.Default.WindowTop = this.Top;
+        }
+    }
+}
